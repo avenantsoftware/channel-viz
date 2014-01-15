fb1ff1b5429bf60093131db996a509289bcbcece
spambus <spambus@gmail.com>
2014-01-15 09:25:12 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -148,7 +148,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
 										height: 200,
-										renderer: 'bar',
+										renderer: 'scatterplot',
 										color: 'steelblue',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
@@ -158,10 +158,7 @@
 											bottom: 0.02,
 											left: 0.02
 										},
-										series: [{
-		                                                                       data: [ { x: 0, y: 40 }, { x: 1, y: 49 }, ...
-		                                                                       color: 'steelblue'
-	                                                                                }]
+										series: series
 									});
 
 									graph.render();
